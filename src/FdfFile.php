b2ec5257b0a4070b22247e687d96271112394a26
Michael Härtl <haertl.mike@gmail.com>
2014-10-15 11:01:26 +0200

    Start 0.2.2-dev

--- src/FdfFile.php
+++ src/FdfFile.php
@@ -10,7 +10,7 @@ use mikehaertl\tmp\File;
  * with valid unicode characters.
  *
  * @author Michael Härtl <haertl.mike@gmail.com>
- * @version 0.2.1
+ * @version 0.2.2-dev
  * @license http://www.opensource.org/licenses/MIT
  */
 class FdfFile extends File
