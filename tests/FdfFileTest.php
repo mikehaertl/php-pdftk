1bc71a8fa6a5ae5b44c94bac579ccc7cf05055de
TobiasBaeumer@gmail.com <TobiasBaeumer@gmail.com>
2015-04-17 17:43:04 +0200

    Tests > FdfFile >> Slightly changed data array used by testFdfFileCreation()

--- tests/FdfFileTest.php
+++ tests/FdfFileTest.php
@@ -5,12 +5,15 @@ class FdfFileTest extends \PHPUnit_Framework_TestCase
 {
     public function testFdfFileCreation() {
         $data = array(
-            "standard" => "nothing special here",
-            "umlauts-in-value" => "öäüÖÄÜ",
-            "öäüÖÄÜ" => "umlauts in key",
+            'name' => 'Jürgen čárka čČćĆđĐ мирано',
+            'email' => 'test@email.com',
             'checkbox 1' => 'Yes',
             'checkbox 2' => 0,
             'radio 1' => 2,
+            "umlauts-in-value" => "öäüÖÄÜ",
+            "öäüÖÄÜ" => "umlauts in key",
+            "special-in-value" => "€ß&()",
+            "€ key" => "special in key",
         );
 
         $oFdfFile = new FdfFile($data, null, null, __DIR__);
