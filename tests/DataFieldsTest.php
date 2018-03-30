70ef3a0387493e5ebcf13b56ab39c421d0f6c7c4
Michael Härtl <haertl.mike@gmail.com>
2018-03-30 09:47:20 +0200

    Issue #117 Fix datafield parsing with warnings

--- tests/DataFieldsTest.php
+++ tests/DataFieldsTest.php
@@ -6,8 +6,13 @@ class DataFieldsTest extends \PHPUnit\Framework\TestCase
     public function testDataFieldParsing()
     {
         $dataFields = new DataFields($this->_testInput);
-        //print_r($dataFields->__toArray());exit;
         $this->assertEquals($this->_parsedResult, $dataFields->__toArray());
+    }
+
+    public function testParsingOfDataFieldWithWarning()
+    {
+        $dataFields = new DataFields($this->_warningInput);
+        $this->assertEquals($this->_parsedWarningResult, $dataFields->__toArray());
     }
 
     protected $_testInput = <<<DATA
@@ -59,6 +64,7 @@ FieldValueDefault: default:with:colons
 ---more:colons:
 and
 multi lines
+   even indented
 
 FieldJustification: Left
 DATA;
@@ -101,8 +107,46 @@ DATA;
             'FieldNameAlt' => 'field5_alt',
             'FieldFlags' => 0,
             'FieldValue' => "field:with:colons\n\n---more:colons:\nand\nmulti lines\n",
-            'FieldValueDefault' => "default:with:colons\n\n---more:colons:\nand\nmulti lines\n",
+            'FieldValueDefault' => "default:with:colons\n\n---more:colons:\nand\nmulti lines\n   even indented\n",
             'FieldJustification' => 'Left',
         ]
+    ];
+
+    protected $_warningInput = <<<DATA
+WARNING: The creator of the input PDF:
+   example_pdf_with_password.pdf
+   has set an owner password (which is not required to handle this PDF).
+   You did not supply this password. Please respect any copyright.
+---
+FieldType: Text
+FieldName: field1
+FieldNameAlt: field1_alt
+FieldFlags: 0
+FieldJustification: Left
+---
+FieldType: Text
+FieldName: field2
+FieldNameAlt: field2_alt
+FieldFlags: 0
+FieldValue: value:with:colons
+FieldJustification: Left
+DATA;
+
+    protected $_parsedWarningResult = [
+        [
+            'FieldType' => 'Text',
+            'FieldName' => 'field1',
+            'FieldNameAlt' => 'field1_alt',
+            'FieldFlags' => 0,
+            'FieldJustification' => 'Left',
+        ],
+        [
+            'FieldType' => 'Text',
+            'FieldName' => 'field2',
+            'FieldNameAlt' => 'field2_alt',
+            'FieldFlags' => 0,
+            'FieldValue' => 'value:with:colons',
+            'FieldJustification' => 'Left',
+        ],
     ];
 }
